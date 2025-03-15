8434c05081a2ff587958b38ab5f1719b4685bd33
Lordimini <lordimini@gmail.com>
2025-03-15 21:03:40 +0100

    d

--- flask_app/routes/auth.py
+++ flask_app/routes/auth.py
@@ -2,62 +2,11 @@ from flask import Blueprint, render_template, request, redirect, url_for, sessio
 from flask_app.models.edir_model import EDIRModel
 from flask_app.utils.ldap_utils import login_required
 from flask_app.config.meta_config import meta_login_config
+from flask_app.services.login_manager import authenticate_user
 
 auth_bp = Blueprint('auth', __name__)
 
-# @auth_bp.route('/login', methods=['GET', 'POST'])
-# def login():
-#     if request.method == 'POST':
-#         username = request.form['username']
-#         user_password = request.form['password']
-        
-#         # Always use META for authentication
-#         ldap_model = EDIRModel(source='meta')
 
-#         # Construct the full user DN
-#         user_dn = f'cn={username},{ldap_model.actif_users_dn}'
-        
-#         # Capture the connection object returned by authenticate
-#         conn = ldap_model.authenticate(username, user_password)
-
-#         if conn:
-#             # Check if the user is a member of the admin group or reader group
-#             is_admin = False
-#             is_reader = False
-            
-#             # Check membership in admin_group_dn
-#             admin_group_dn = ldap_model.admin_group_dn
-#             reader_group_dn = ldap_model.reader_group_dn
-            
-#             # Check if user is in admin group
-#             conn.search(admin_group_dn, f'(member={conn.user})', search_scope='BASE')
-#             if conn.entries:
-#                 is_admin = True
-
-#             # If not admin, check if user is in reader group
-#             if not is_admin:
-#                 conn.search(reader_group_dn, f'(member={conn.user})', search_scope='BASE')
-#                 if conn.entries:
-#                     is_reader = True
-                    
-#             # Grant access if the user is a member of either group
-#             if is_admin or is_reader:
-#                 session['logged_in'] = True
-#                 session['username'] = username
-#                 session['role'] = 'admin' if is_admin else 'reader'  # Store role in session
-                
-#                 # Set the active LDAP source to META by default
-#                 session['ldap_source'] = 'meta'
-#                 session['ldap_name'] = meta_login_config.get('LDAP_name', 'META')
-                
-#                 flash('Login successful!', 'success')
-#                 return redirect(url_for('dashboard.dashboard'))
-#             else:
-#                 flash('You are not authorized to access this application.', 'danger')
-#         else:
-#             flash('Invalid username or password!', 'danger')
-    
-#     return render_template('login.html')
 @auth_bp.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -90,6 +39,8 @@ def login():
 
 @auth_bp.route('/logout')
 def logout():
+    from flask_login import logout_user
+    logout_user()  # Déconnecte l'utilisateur avec Flask-Login
     session.clear()
     flash('You have been logged out.', 'success')
     return redirect(url_for('auth.login'))

e4afc3803768d4a548895833ed2b7a0f41bae8b3
Lordimini <lordimini@gmail.com>
2025-03-24 22:58:15 +0100

    sd

--- flask_app/routes/postcreation.py
+++ flask_app/routes/postcreation.py
@@ -33,7 +33,7 @@ def post_creation():
     
     # Get all pending users
     options = {
-                'container': 'toprocess',
+                'container': 'all',
                 'return_list': True
                 }
     all_pending_users = ldap_model.get_user("(objectClass=Person)", options)
